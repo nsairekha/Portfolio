73795b478a4d2a0973849b251242b2b6e0326269
nsairekha <rekhans321@gmail.com>
2026-02-16 00:07:11 +0530

    V1.0.0

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './footer.css'
+
+export const footer = () => {
+  return (
+        <div className="FooterComponent">
+            <div className="FooterComponent-in">
+                 <p>© 2025 Nadukuru Sai Rekha. All rights reserved.</p>
+            </div>
+        </div>
+  )
+}
+
+
+export default footer
---
+++ src/app/ui/projects/page.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import Link from 'next/link'
+
+import './page.css'
+
+import { RiExternalLinkLine } from "react-icons/ri";
+import { CiGlobe } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
+
+// Define the project type
+interface Project {
+    title: string;
+    description: string;
+    techStack: string[];
+    liveLink: string;
+    sourceLink: string;
+}
+
+// Projects data array
+const projects: Project[] = [
+    {
+        title: "sac.kluniversity.in",
+        description: "Student Activity Center Website, K L Deemed to be University",
+        techStack: ["Next.js", "CSS", "TypeScript", "MySQL", "Docker", "Redis", "NGINX"],
+        liveLink: "https://sac.kluniversity.in",
+        sourceLink: "https://github.com/saitadikonda99/Kepler-452b"
+    },
+    
+    
+    {
+        title: "Personal Portfolio",
+        description: "My Personal Portfolio",
+        techStack: ["Next.js", "TypeScript",],
+        liveLink: "https://sairekhanadukuru.vercel.app/",
+        sourceLink: "https://github.com/nsairekha/Portfolio.git"
+    }
+
+];
+
+const page = () => {
+    return (
+        <div className="ProjectsComponent" id="projects">
+            <div className="ProjectsComponent-in">
+                <div className="projects-one">
+                    <h1>Projects</h1>
+                </div>
+                {projects.map((project, index) => (
+                    <div className="Project-Card" key={index}>
+                        <div className="Project-Card-in">
+                            <div className="project-card-one">
+                                <Link href={project.liveLink} target='_blank'>
+                                    {project.title} <RiExternalLinkLine />
+                                </Link>
+                            </div>
+                            <div className="project-card-two">
+                                <p>{project.description}</p>
+                            </div>
+                            <div className="project-card-three">
+                                {project.techStack.map((tech, techIndex) => (
+                                    <div className="Project-stack" key={techIndex}>
+                                        <p>{tech}</p>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="project-card-four">
+                                <Link href={project.liveLink} target='_blank'><CiGlobe /> Live</Link>
+                                <Link href={project.sourceLink} target='_blank'><FaGithub />Source</Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default page
